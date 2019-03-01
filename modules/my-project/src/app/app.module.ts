f565e3526059725603fe13c36e54e8dcfdc217e6
Travis Cory <travis.cory@liferay.com>
2019-02-28 16:44:29 -0800

    Add material button module

--- modules/my-project/src/app/app.module.ts
+++ modules/my-project/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
+	MatButtonModule,
 	MatCardModule,
 	MatCheckboxModule
 } from '@angular/material';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
 	imports: [
 		BrowserAnimationsModule,
 		BrowserModule,
+		MatButtonModule,
 		MatCardModule,
 		MatCheckboxModule
 	],
