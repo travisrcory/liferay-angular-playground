926bb05078b8157a8ca1e3e8b5e16ff9581a5b24
Alex Valencia <alexander.valencia@liferay.com>
2019-03-05 16:19:39 -0800

    Add example for testing nested component

--- modules/my-project/src/app/app.component.spec.ts
+++ modules/my-project/src/app/app.component.spec.ts
@@ -1,30 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
-import { MatCheckboxModule } from '@angular/material';
+import { CardComponent } from './components/card/card.component';
+import {
+	MatButtonModule,
+	MatCardModule,
+	MatCheckboxModule,
+} from '@angular/material';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        MatCheckboxModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [AppComponent, CardComponent],
+			imports: [MatButtonModule, MatCardModule, MatCheckboxModule],
+		}).compileComponents();
+	}));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app).toBeTruthy();
+	});
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome!');
-  });
+	it('should have the title "Welcome!"', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app.title).toEqual('Welcome!');
+	});
+
+	it('should render title in a h1 tag', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('h1').textContent).toContain('Welcome!');
+	});
 });
